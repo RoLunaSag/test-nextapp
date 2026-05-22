4265271c473f88b10bf1982be8b31186cff3c64b
rodrigo luna <rodrigo.luna@ial.com.mx>
2026-05-22 03:40:30 -0600

    Feature: Agregando el type del CartItemRowProps

--- src/types/componentTypes.ts
+++ src/types/componentTypes.ts
@@ -1,7 +1,15 @@
 import { Product } from "@/models/prodcut";
+import { CartItem } from "@/models/cart";
 
 export type ProductCardProps = {
     product: Product;
     onClick?: () => void;
     onAddToCart?: () => void;
+};
+
+export type CartItemRowProps = {
+    item: CartItem;
+    onIncrement: () => void;
+    onDecrement: () => void;
+    onRemove: () => void;
 };
