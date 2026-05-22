f7e72a60807be7545462190f5bf871f14fd4aefb
rodrigo luna <rodrigo.luna@ial.com.mx>
2026-05-22 03:32:40 -0600

    Feature: Agregando el carrito a la Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,34 @@
 'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useCartCount } from '@/stores/useCartStore';
 
 export default function Navbar() {
+    const count = useCartCount();
+    const [hydrated, setHydrated] = useState(false);
+
+    useEffect(() => {
+        setHydrated(true);
+    }, []);
+
     return (
         <nav className="bg-gray-800 p-4">
-            <ul className="flex space-x-4">
+            <ul className="flex space-x-4 items-center">
                 <li>
-                    <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+                    <Link href="/" className="text-white hover:text-gray-300">
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/products" className="text-white hover:text-gray-300">Products</Link>
+                    <Link href="/products" className="text-white hover:text-gray-300">
+                        Products
+                    </Link>
+                </li>
+                <li className="ml-auto">
+                    <Link href="/cart" className="text-white hover:text-gray-300">
+                        🛒 Cart ({hydrated ? count : 0})
+                    </Link>
                 </li>
             </ul>
         </nav>
