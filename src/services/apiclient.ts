eacfeecadbd13f1bebeb4b6f7a4ca4a3ea56038d
rodrigo luna <rodrigo.luna@ial.com.mx>
2026-05-22 03:15:10 -0600

    Feature: Agregando la funcion asyncrona de fetchProduct al apiclient

--- src/services/apiclient.ts
+++ src/services/apiclient.ts
@@ -1,9 +1,15 @@
-import type { ProductList } from '@/models/prodcut';
+import type { Product, ProductList } from '@/models/prodcut';
 
 const BASE_URL = 'https://fakestoreapi.com';
 
 export async function fetchProducts(): Promise<ProductList> {
     const res = await fetch(`${BASE_URL}/products`);
     if (!res.ok) throw new Error('Failed to fetch products');
+    return res.json();
+}
+
+export async function fetchProduct(id: string | number): Promise<Product> {
+    const res = await fetch(`${BASE_URL}/products/${id}`);
+    if (!res.ok) throw new Error(`Failed to fetch product ${id}`);
     return res.json();
 }
